8da5c9bf8d8a602637ffc8503c96e74a9c00fa08
JuhJSimoes <julianajsimoes@gmail.com>
2022-11-01 05:01:17 -0300

    Aula_011122_Series_Index Estudo de serie e index, iniciando filtro de dados.

--- extras/criando_estruturas_de_dados.py
+++ extras/criando_estruturas_de_dados.py
@@ -33,3 +33,43 @@ print(data, '\n')
 
 df1 = pd.DataFrame(data = data)
 print(df1, '\n')
+
+index = ['Linha ' + str(i) for i in range(3)]
+print(index, '\n')
+df1 = pd.DataFrame(data = data, index = index)
+print(df1, '\n')
+
+columns = ['Coluna ' + str(i) for i in range(3)]
+print(columns, '\n')
+
+df1 = pd.DataFrame(data = data, index = index, columns = columns)
+print(df1,'\n')
+
+data = {'Coluna 0': {'Linha 0': 1, 'Linha 1': 4, 'Linha 2': 7},
+        'Coluna 1': {'Linha 0': 2, 'Linha 1': 5, 'Linha 2': 8},
+        'Coluna 2': {'Linha 0': 3, 'Linha 1': 6, 'Linha 2': 9}}
+df2 = pd.DataFrame(data)
+print(df2, '\n')
+
+data = [(1, 2, 3), 
+        (4, 5, 6), 
+        (7, 8, 9)]
+df3 = pd.DataFrame(data = data, index = index, columns = columns)
+print(df3, '\n')
+
+df1[df1 > 0] = 'A'
+print(df1, '\n')
+
+df2[df2 > 0] = 'B'
+print(df2, '\n')
+
+df3[df3 > 0] = 'C'
+print(df3, '\n')
+
+df4 = pd.concat([df1, df2, df3])
+print(df4, '\n')
+
+df4 = pd.concat([df1, df2, df3], axis = 1)
+print(df4, '\n')
+
+
